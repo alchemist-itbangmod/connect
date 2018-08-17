8c9070c081948e3251d523c7c80b330b81901246
Kanisorn S <firstziiz.k@gmail.com>
2018-08-17 19:40:46 +0700

    Remove Component to use just one Login.js

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,11 +1,58 @@
 import React from 'react'
-import GoogleLoginButton from '../components/Login/GoogleLoginButton'
+import firebase from 'firebase/app'
+import { withRouter } from 'react-static'
+import { Button } from 'antd'
 
-const LoginPage = () => (
-  <div className="container h-100 d-flex justify-content-center align-items-center flex-column">
-    <p className="small">Please Login with your KMUTT Email, Let's enjoy!</p>
-    <GoogleLoginButton />
-  </div>
-)
+class LoginPage extends React.Component {
+  state = {
+    loading: true
+  }
 
-export default LoginPage
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(async authUser => {
+      if (authUser) {
+        await this.props.history.push('/')
+      }
+      this.setState({ loading: false })
+    })
+  }
+
+  login = () => {
+    this.setState({
+      loading: true
+    })
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    try {
+      firebase.auth().signInWithRedirect(provider)
+    } catch (error) {
+      // Handle Error here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      // const email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      // const credential = error.credential;
+
+      console.log(`${errorCode}: ${errorMessage}`)
+      this.setState({
+        loading: false
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="container h-100 d-flex justify-content-center align-items-center flex-column">
+        <p className="small">
+          Please Login with your KMUTT Email, Let's enjoy!
+        </p>
+        <Button onClick={this.login} size="large" loading={this.state.loading}>
+          Login with Google
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default withRouter(LoginPage)
