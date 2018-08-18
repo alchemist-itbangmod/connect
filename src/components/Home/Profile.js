fbbd9b74018767105e734aa314d84cfeef8c1c87
Kanisorn S <firstziiz.k@gmail.com>
2018-08-19 03:30:36 +0700

    User can add IT student ID & only login with KMUTT Email

--- src/components/Home/Profile.js
+++ src/components/Home/Profile.js
@@ -1,15 +1,23 @@
 import React from 'react'
-import { Avatar, Button, Input, message } from 'antd'
+import { Avatar, Button, Input, message, Modal } from 'antd'
 import { connect } from 'react-redux'
 import * as R from 'ramda'
 
 import { capitalizeFirstLetter } from '../../libs/capitalize-first-letter'
 import UploadAvatar from './UploadAvatar'
 import { setUser } from '../../firebase/data'
+
+const confirm = Modal.confirm
+
+const getYear = year => {
+  if (year <= 57) return 'Senior'
+  else return 62 - year
+}
+
 class Profile extends React.Component {
   state = {
     loading: false,
-    stdID: '58130500009',
+    stdID: this.props.stdID || '',
     nickName: this.props.nickName || '',
     bio: this.props.bio || ''
   }
@@ -17,6 +25,38 @@ class Profile extends React.Component {
   setField = (field, value) => this.setState({ [field]: value })
 
   handleUpdateProfile = async () => {
+    // Update Student ID
+    if (
+      this.state.stdID !== '' &&
+      (R.isEmpty(this.props.stdID) || R.isNil(this.props.stdID))
+    ) {
+      if (this.state.stdID.length !== 11) {
+        message.error('Your Student ID is not valid')
+        return
+      }
+
+      const year = this.state.stdID.substring(0, 2)
+      console.log(year)
+      if (+year > 61 || +year < 57) {
+        message.error('Your study level is wrong.')
+        return
+      }
+
+      confirm({
+        title: 'คุณจะสามารถตั้งรหัสนักศึกษาได้เพียงครั้งเดียว',
+        content: `คุณจะสามารถตั้งรหัสนักศึกษาได้เพียงครั้งเดียว, รหัสนักศึกษา ${
+          this.state.stdID
+        } นี้เป็นของคุณหรือไม่ ?`,
+        onOk: async () => {
+          await setUser(this.props.userInfo.uid, {
+            ...R.pick(['stdID'], this.state),
+            level: getYear(year)
+          })
+        }
+      })
+    }
+
+    // Update nickname & bio
     this.setState({
       loading: true
     })
@@ -56,8 +96,11 @@ class Profile extends React.Component {
             <Input
               className="mb-1"
               placeholder="Student ID"
-              disabled
-              value={`58130500009`}
+              disabled={
+                !R.isEmpty(this.props.stdID) && !R.isNil(this.props.stdID)
+              }
+              onChange={e => this.setField('stdID', e.target.value)}
+              value={this.state.stdID}
             />
             <Input
               className="mb-1"
