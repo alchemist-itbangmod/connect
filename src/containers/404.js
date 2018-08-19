870113f4840d6417761ce8bcbf8e7f3ebec28fd3
Kanisorn S <firstziiz.k@gmail.com>
2018-08-20 02:28:16 +0700

    finish 404

--- src/containers/404.js
+++ src/containers/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 //
 
 export default () => (
-  <div className="d-flex justify-content-center align-items-center">
-    <h1>404 | เข้ามาผิดหน้าแล้วรู้ไหม?</h1>
+  <div className="d-flex justify-content-center align-items-center text-center h-100">
+    <h6>404 | เข้ามาผิดหน้าแล้วรู้ไหม?</h6>
   </div>
 )
